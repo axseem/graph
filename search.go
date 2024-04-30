7e053e6fc0960e9f8a5c534dd13284386b706a7d
axseem <tr1xn.gg@gmail.com>
2024-04-30 19:25:17 +0200

    feat: iteration function for bfs and dfs

--- search.go
+++ search.go
@@ -1,54 +1,76 @@
 package graph
 
 import (
-	"cmp"
+	"log"
 )
 
-func DFS[K cmp.Ordered](g Graph[K], entry K) []K {
-	if g.Adjacency(entry) == nil {
-		return nil
-	}
-
-	// empty struct doesn't take space
-	visited := make(map[K]struct{})
-	output := []K{entry}
-	dfs(g, entry, &visited, &output)
-	return output
-}
-
-func dfs[K cmp.Ordered](g Graph[K], entry K, visited *map[K]struct{}, output *[]K) {
-	(*visited)[entry] = struct{}{}
-	n := g.Adjacency(entry)
-	for _, v := range n {
-		if _, ok := (*visited)[v]; !ok {
-			*output = append(*output, v)
-			dfs(g, v, visited, output)
-		}
-	}
-}
-
-func BFS[K cmp.Ordered](g Graph[K], entry K) []K {
+// Each time a vertex is visited, the while function is triggered.
+// The function exits if while returns false or there are no more vertices.
+func BFS[K comparable](g Graph[K], entry K, while func(vertex K, depth uint) bool) error {
 	queue := []K{entry}
 	visited := make(map[K]struct{})
-	var output []K
+	var depth, depthCounter uint
+	var depthThreshold uint = 1
 
 	for len(queue) > 0 {
-		bot := queue[0]
+		bottom := queue[0]
 		queue = queue[1:]
 
-		if _, ok := visited[bot]; !ok {
+		if _, ok := visited[bottom]; ok {
+			depthThreshold--
 			continue
 		}
 
-		output = append(output, bot)
-		n := g.Adjacency(bot)
-		if n == nil {
+		if !while(bottom, depth) {
 			return nil
 		}
 
+		n := g.Adjacency(bottom)
+		if n == nil {
+			return ErrNilVertex
+		}
+
 		queue = append(queue, n...)
-		visited[bot] = struct{}{}
+		visited[bottom] = struct{}{}
+
+		depthCounter++
+		if depthCounter == depthThreshold {
+			depth++
+			depthCounter = 0
+			depthThreshold = uint(len(queue))
+		}
+		log.Println(depthThreshold, depthCounter)
 	}
 
-	return output
+	return nil
+}
+
+// Each time a vertex is visited, the while function is triggered.
+// The function exits if while returns false or there are no more vertices.
+func DFS[K comparable](g Graph[K], entry K, while func(vertex K) bool) error {
+	queue := []K{entry}
+	visited := make(map[K]struct{})
+
+	for len(queue) > 0 {
+		top := queue[len(queue)-1]
+		queue = queue[:len(queue)-1]
+
+		if _, ok := visited[top]; ok {
+			continue
+		}
+
+		if !while(top) {
+			return nil
+		}
+
+		n := g.Adjacency(top)
+		if n == nil {
+			return ErrNilVertex
+		}
+
+		queue = append(queue, n...)
+		visited[top] = struct{}{}
+	}
+
+	return nil
 }
--- search_test.go
+++ search_test.go
@@ -7,20 +7,24 @@ import (
 	"github.com/axseem/graph"
 )
 
-func TestDFS(t *testing.T) {
-	testCases := []struct {
-		desc   string
-		order  uint
-		edges  [][2]uint
-		entry  uint
-		output []uint
-	}{
+type test struct {
+	desc   string
+	order  uint
+	edges  [][2]uint
+	entry  uint
+	output []uint
+	err    error
+}
+
+func commonTests() []test {
+	return []test{
 		{
 			desc:   "empty graph",
 			order:  0,
 			edges:  nil,
 			entry:  0,
 			output: nil,
+			err:    graph.ErrNilVertex,
 		},
 		{
 			desc:   "trivial graph",
@@ -64,14 +68,19 @@ func TestDFS(t *testing.T) {
 			entry:  0,
 			output: []uint{0, 1, 2},
 		},
+	}
+}
+
+func TestDFS(t *testing.T) {
+	testCases := append(commonTests(), []test{
 		{
-			desc:   "graph:2←1←0→3→4",
+			desc:   "graph:4←3←0→1→2",
 			order:  7,
-			edges:  [][2]uint{{1, 2}, {0, 1}, {0, 3}, {3, 4}},
+			edges:  [][2]uint{{3, 4}, {0, 3}, {0, 1}, {1, 2}},
 			entry:  0,
 			output: []uint{0, 1, 2, 3, 4},
 		},
-	}
+	}...)
 
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
@@ -85,10 +94,116 @@ func TestDFS(t *testing.T) {
 				panic(err)
 			}
 
-			output := graph.DFS(g, tC.entry)
-			if !reflect.DeepEqual(output, tC.output) {
-				t.Errorf("expected: %v, got: %v", tC.output, output)
+			path := []uint{}
+			err := graph.DFS(g, tC.entry, func(vertex uint) bool {
+				path = append(path, vertex)
+				return true
+			})
+			if err != nil {
+				if err == tC.err {
+					return
+				}
+				panic(err)
+			}
+
+			if !reflect.DeepEqual(tC.output, path) {
+				t.Errorf("expected: %v, got: %v", tC.output, path)
 			}
 		})
+	}
+}
+
+func TestBFS(t *testing.T) {
+	testCases := append(commonTests(), []test{
+		{
+			desc:   "graph:3←1←0→2→4",
+			order:  7,
+			edges:  [][2]uint{{1, 3}, {0, 1}, {0, 2}, {2, 4}},
+			entry:  0,
+			output: []uint{0, 1, 2, 3, 4},
+		},
+	}...)
+
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			g := graph.NewIndexed[uint]()
+
+			if err := g.AddVertices(tC.order); err != nil {
+				panic(err)
+			}
+
+			if err := g.AddEdges(tC.edges...); err != nil {
+				panic(err)
+			}
+
+			path := []uint{}
+			err := graph.BFS(g, tC.entry, func(vertex, depth uint) bool {
+				path = append(path, vertex)
+				return true
+			})
+			if err != nil {
+				if err == tC.err {
+					return
+				}
+				panic(err)
+			}
+
+			if !reflect.DeepEqual(tC.output, path) {
+				t.Errorf("expected: %v, got: %v", tC.output, path)
+			}
+		})
+	}
+}
+
+func TestBFSDepth(t *testing.T) {
+	g := graph.NewIndexed[uint]()
+
+	if err := g.AddVertices(7); err != nil {
+		panic(err)
+	}
+
+	if err := g.AddEdges([][2]uint{{1, 2}, {0, 1}, {0, 3}, {3, 4}}...); err != nil {
+		panic(err)
+	}
+
+	path := []uint{}
+	err := graph.BFS(g, 0, func(vertex, depth uint) bool {
+		if depth > 1 {
+			return false
+		}
+
+		path = append(path, vertex)
+		return true
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	expect := []uint{0, 1, 3}
+	if !reflect.DeepEqual(expect, path) {
+		t.Errorf("expected: %v, got: %v", expect, path)
+	}
+}
+
+func TestBFSDepthGrid(t *testing.T) {
+	g := graph.NewGrid()
+
+	amount := 0
+	err := graph.BFS(g, [2]int{0, 0}, func(vertex [2]int, depth uint) bool {
+		if depth > 2 {
+			return false
+		}
+
+		// log.Println(vertex)
+		amount++
+		return true
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	expect := 13
+	if !reflect.DeepEqual(expect, amount) {
+		t.Errorf("expected: %v, got: %v", expect, amount)
 	}
 }
